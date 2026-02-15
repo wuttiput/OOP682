66f0274985a25c80d47044671eec78a27641d030
wuttiput <wuttiput00002@gmail.com>
2026-02-15 14:23:26 +0700

    Delete Week09 directory

--- Week09/models/student.py
+++
@@ -1,9 +0,0 @@
-from models.person import Person
-
-class student(Person):
-    def __init__(self,pid, name, age,student_id):
-        super().__init__(pid,name, age)
-        self.student_id = student_id
-
-    def __str__(self):
-        return super().__str__() + f', StudentID: {self.student_id}'
